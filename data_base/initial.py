3af96f7f390afe11c1d9a2608c62b32874bbd953
Victor Salustrino Bezerra <salustrino@gmail.com>
2023-11-12 18:19:14 -0300

    Make the insertion of 5 products in database.

--- data_base/initial.py
+++ data_base/initial.py
@@ -7,23 +7,38 @@ import sqlite3 as sql
 
 DROP_TABLE_PRODUCTS = "DROP TABLE IF EXISTS products;"
 
-CREATE_TABLE_PRODUCTS = "CREATE TABLE products ("\
-    "id INTEGER, name VARCHAR(80), brand VARCHAR(80), price REAL );"
+CREATE_TABLE_PRODUCTS = """
+    CREATE TABLE products (
+        id INTEGER, 
+        name VARCHAR(80), 
+        brand VARCHAR(80), 
+        price REAL 
+    );"""
 
-INSERT_PRODUCTS = "INSERT INTO products (id, name, brand, price)"\
-    "VALUES (1, 'Macarrão', 'Adria', 3.99);"
-
+INSERT_PRODUCTS = """
+    INSERT INTO products (id, name, brand, price)
+    VALUES 
+    (1, 'Sparkling water', 'S. Pellegrino', 3.99),
+    (2, 'Weath flour', 'Caputo', 1.99),
+    (3, 'Olive', 'Olivere', 5.99),
+    (4, 'Cheddar', 'Joseph Heler', 2.99),
+    (5, 'Pringles Original', 'Pringles', 2.49);
+    """
 
 conn = sql.connect('database.db')
-print("Database connected")
+print("Database connected.")
 
 conn.execute(DROP_TABLE_PRODUCTS)
-print("Table dropped")
+print("Table dropped.")
 
 conn.execute(CREATE_TABLE_PRODUCTS)
-print("Table created")
+print("Table created.")
 
-# conn.execute(INSERT_PRODUCTS)
-# print("Row inserted")
+with sql.connect("database.db") as con:
+    cur = con.cursor()
+    cur.execute(INSERT_PRODUCTS)
+
+    con.commit()
+    print("Rows inserted.")
 
 conn.close()
