3b5aa65acce83e16d84f8159dec863e929f66c60
Victor Salustrino Bezerra <salustrino@gmail.com>
2023-11-19 18:49:46 -0300

    Changing DAO's properties from public to private.

--- dao/dao.py
+++ dao/dao.py
@@ -1,6 +1,7 @@
 """
     Data Access Object - DAO
-    A generic class for others DAO
+    Class responsible for execute SQL instructions
+    and handle connections with DataBase.
 """
 
 import sqlite3 as sql
@@ -9,23 +10,23 @@ class DAO:
     """ Generic DAO """
 
     def __init__(self, db_file):
-        self.db_file = db_file
-        self.connection = None
+        self.__db_file = db_file
+        self.__connection = None
 
     def __enter__(self):
-        self.connection = sql.connect(self.db_file)
+        self.__connection = sql.connect(self.__db_file)
         return self
 
     def __exit__(self, *args):
-        self.connection.close()
+        self.__connection.close()
 
     def select_one(self, sql_query):
         """
             Performs a SELECT sql query and
             returns only one row.
         """
-        self.connection.row_factory = sql.Row
-        cursor = self.connection.cursor()
+        self.__connection.row_factory = sql.Row
+        cursor = self.__connection.cursor()
         cursor.execute(sql_query)
 
         return cursor.fetchone()
@@ -35,22 +36,25 @@ class DAO:
             Performs a SELECT sql query and
             returns many rows.
         """
-        self.connection.row_factory = sql.Row
-        cursor = self.connection.cursor()
+        self.__connection.row_factory = sql.Row
+        cursor = self.__connection.cursor()
         cursor.execute(sql_query)
 
         return cursor.fetchall()
 
-    def data_change(self, sql_query):
+    def __data_change(self, sql_query):
         """
             Performs SQL commands that make changes on data
             and them persists the changes through a commit.
             Used to perform INSERT, UPDATE and DELETE.
+            NOTE: for a better readability, use its aliases,
+            dependending on the SQL instruction it's being 
+            executed
         """
-        cur = self.connection.cursor()
+        cur = self.__connection.cursor()
         cur.execute(sql_query)
-        self.connection.commit()
+        self.__connection.commit()
 
     # Different aliases for data_change()
     # to make the code more readable.
-    insert = update = delete = data_change
+    insert = update = delete = __data_change
